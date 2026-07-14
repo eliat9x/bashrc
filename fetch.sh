0b9c585f3dd9631feddb011879235d6bf696e926
eliat9x <eliat9x@gmail.com>
2026-07-13 21:52:52 -0400

    modify fetch.sh script

--- fetch.sh
+++ fetch.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-echo "copy ~/.bashrc.user to here...."
+echo "Copying ~/.bashrc.user to here...."
 cp ~/.bashrc.user .
-echo "copy ~/.tmux.conf to here...."
+
+echo "Copying ~/.tmux.conf to here...."
 cp ~/.tmux.config .
-echo "copy ~/.config/nvim/init.lua to here...."
+
+echo "Copying ~/.config/nvim/init.lua to here...."
 cp ~/.config/nvim/init.lua .
 
 echo "finished"
